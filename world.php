d4fec94173c177f19b5bc6b120bde077a0f684ce
alyanaduncan04 <alyanaduncan1@gmail.com>
2024-11-23 21:58:01 -0500

    Added Addtional Country Information Table Display

--- world.php
+++ world.php
@@ -16,22 +16,36 @@ try {
 
 if (isset($_GET['country'])) {
     $country = $_GET['country'];
-    echo "Country parameter: " . htmlspecialchars($country) . "<br>";
 
-    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+    $stmt = $conn->prepare("SELECT name, continent, independence_year, head_of_state FROM countries WHERE name LIKE :country");
     $stmt->execute(['country' => "%$country%"]);
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     if ($results) {
-        echo "<ul>";
+        echo "<table border='1' style='width: 100%; border-collapse: collapse;'>";
+        echo "<thead>";
+        echo "<tr>";
+        echo "<th>Name</th>";
+        echo "<th>Continent</th>";
+        echo "<th>Independence Year</th>";
+        echo "<th>Head of State</th>";
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
         foreach ($results as $row) {
-            echo "<li>" . htmlspecialchars($row['name']) . " is ruled by " . htmlspecialchars($row['head_of_state']) . "</li>";
+            echo "<tr>";
+            echo "<td>" . htmlspecialchars($row['name']) . "</td>";
+            echo "<td>" . htmlspecialchars($row['continent']) . "</td>";
+            echo "<td>" . htmlspecialchars($row['independence_year']) . "</td>";
+            echo "<td>" . htmlspecialchars($row['head_of_state']) . "</td>";
+            echo "</tr>";
         }
-        echo "</ul>";
+        echo "</tbody>";
+        echo "</table>";
     } else {
-        echo "No results found for '$country'.";
+        echo "<p>No results found for '" . htmlspecialchars($country) . "'</p>";
     }
 } else {
-    echo "No country parameter provided.";
+    echo "<p>Please specify a country using the 'country' query parameter.</p>";
 }
 ?>
